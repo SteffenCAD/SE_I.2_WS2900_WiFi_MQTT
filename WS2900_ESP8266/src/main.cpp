3475d48fe7c1c0a7d2b604a66d025c7245104387
SteffenCAD <31154486+SteffenCAD@users.noreply.github.com>
2023-12-24 16:19:30 +0100

    - change platform IO settings -> esp_wroom_02 (2mb flash), uploadspeed - use elegantOTA v3 - change serialports from software to hardware, add software debug port
    - add function to enable ota mode via command or with pin on startup
    - reset esp when entering ota mode, use rtc storage to store the flag
    
    - geral testing on real hardware
    
    - bugfixes

--- WS2900_ESP8266/src/main.cpp
+++ WS2900_ESP8266/src/main.cpp
@@ -1,194 +1,236 @@
 #include "main.h"
 
 
+/// @brief setup
+void setup() 
+{
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(OTA_PIN, INPUT_PULLUP);
+  digitalWrite(LED_PIN, HIGH);
 
+  WsSerial.begin(WsBaud);
+  DbgSerial.begin(DbgBaud);
+  
+  DbgSerial.println("--------------------------------------------------------");
+  espClientSec.setInsecure();   //allow unsecure connections (don't compare certificate)
 
+  //OTA-Update stuff
+  ESP.rtcUserMemoryRead(otaEnalbedOff, &otaEnabled, sizeof(otaEnabled));
+  if(otaEnabled == 1 || digitalRead(OTA_PIN) == 0)
+  {
+    DbgSerial.println("Enter OTA-Update mode");
 
-void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(ssid, password);
+    //save state to runtime var to hold in update session
+    otaRunning = true;
+
+    //get system time to reset ota after 1hour
+    initOtaTime = millis();
+
+    //reset rtc memory
+    otaEnabled = 0;
+    ESP.rtcUserMemoryWrite(otaEnalbedOff, &otaEnabled, sizeof(otaEnabled));
+
+    createAp();
+    server.on("/", []() { server.send(200, "text/plain", "Hi! This is ElegantOTA Demo.");});
+    ElegantOTA.begin(&server);    // Start ElegantOTA
+    server.begin();
+    DbgSerial.println("HTTP server started");
+
+  }
+  else
+  {
+    DbgSerial.println("Enter Normal mode");
+    connectToWifi();
+  }
+ 
+  DbgSerial.println("WS Router begin");
+  WsRouter.begin(&WsBuffer, &WsData, &WsSerial, &NtpClient);
+
+  DbgSerial.println("Init complete");
 }
 
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  Serial.println(WiFi.localIP());
 
-  Serial.println("Connect to NTP");
+/// @brief main loop
+void loop() 
+{
+if(!otaRunning)
+  {
+    if (WiFi.isConnected())
+    {
+      //Ntp client will update every 60 seconds
+      NtpClient.update();
+
+      if (serialTimestamp + 10000 < millis())
+      {
+        Serial.print(NtpClient.getFormattedTime());
+        Serial.print("  ");
+        Serial.println(WiFi.localIP());
+        serialTimestamp = millis();
+      }
+
+      //update mqtt client
+      if (mqttEnabled == true)
+      {
+        MqttClient.loop();
+      }
+    }
+
+    //read all bytes to ring buffer
+    while(WsSerial.available() > 0 )
+    {
+      char input = WsSerial.read();
+      WsBuffer.add(input);
+    }
+
+    //weather station routing and responses
+    WsRouter.route();
+
+    //if WsData has new data, print to debug Serial
+    if(WsData.available())
+    {
+      //convert string to char array for publishing
+      String JsonData = WsData.toJson(true);
+      char arr[JsonData.length()+1];
+      JsonData.toCharArray(arr, JsonData.length()+1);
+
+      //print weather data to debug console
+      DbgSerial.println(WsData.toString());
+
+
+      if (WiFi.isConnected())
+      {
+
+        if (mqttEnabled == true)
+        {
+          //publish to mqtt
+          if(MqttClient.connected())
+          {
+            MqttClient.publish(mqtt_topic, arr);
+          }
+          else
+          {
+            MqttClient.connect(mqtt_clientId, mqtt_user, mqtt_pass);
+          }
+        }
+      }
+    }
+
+    //if ota service has to init
+    if(WsRouter.initOTA())
+    {
+      //if we want to update controller, write state to rtc memory and restart esp
+      otaEnabled = 1;
+      ESP.rtcUserMemoryWrite(otaEnalbedOff, &otaEnabled, sizeof(otaEnabled));
+      ESP.restart();
+    }
+  }
+  else
+  {
+    //Handle WiFi stuff
+    server.handleClient();
+    ElegantOTA.loop();
+
+    if(initOtaTime != 0 && (initOtaTime + initOtaTimeout) < millis())
+    {
+      otaRunning = false;
+      initOtaTime = 0;
+
+      DbgSerial.println("Reset otaEnabled flag in RTC storage");
+      otaEnabled = 0;
+      ESP.rtcUserMemoryWrite(otaEnalbedOff, &otaEnabled, sizeof(otaEnabled));
+
+      DbgSerial.println("Reset esp8266");
+      ESP.restart();
+    }
+  }
+}
+
+
+void connectToWifi() 
+{
+  DbgSerial.println("Connecting to Wi-Fi...");
+
+  //wifiConnectHandler    = WiFi.onStationModeGotIP(onWifiConnect);
+  //wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
+
+  WiFi.begin(ssid, password);
+
+
+  DbgSerial.println("Connected to Wi-Fi.");
+  DbgSerial.println(WiFi.localIP());
+
+  DbgSerial.println("Connect to NTP");
   NtpClient.begin();
   NtpClient.setTimeOffset(0);
 
   NtpClient.update();
-  Serial.println(NtpClient.getFormattedTime());
+  DbgSerial.println(NtpClient.getFormattedTime());
 
   if (mqttEnabled == true)
   {
-    Serial.println("init MQTT");
+    DbgSerial.println("init MQTT");
     MqttClient.setServer(mqtt_server, mqtt_port);
     MqttClient.connect(mqtt_clientId, mqtt_user, mqtt_pass);
   }
   else
   {
-    Serial.println("MQTT disabled in main.h");
+    DbgSerial.println("MQTT disabled in main.h");
   }
 
-  Serial.println("pre init OTA");
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", otaMessage);
-    });
+  DbgSerial.println("pre init OTA");
   otaMessage = "WS is in normal operation mode. For updating the firmware please enter OTA update mode and connect to the OTA Hotspot";
+  server.on("/", []() { server.send(200, "text/plain", otaMessage);});
+  server.begin();
+
+}
+
+void createAp()
+{
+  if (WiFi.isConnected())
+  {
+    WiFi.disconnect(true);
+    while (WiFi.status() == WL_CONNECTED) {
+      delay(500);
+      DbgSerial.println("Wait for disconnect...");
+    } 
+  }
+
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAPConfig(local_IP,gateway,subnet);
+  WiFi.softAP(ApSsid, ApPassword);
+}
+
+void onWifiConnect(const WiFiEventStationModeGotIP& event) 
+{
+  DbgSerial.println("Connected to Wi-Fi.");
+  DbgSerial.println(WiFi.localIP());
+
+  DbgSerial.println("Connect to NTP");
+  NtpClient.begin();
+  NtpClient.setTimeOffset(0);
+
+  NtpClient.update();
+  DbgSerial.println(NtpClient.getFormattedTime());
+
+  if (mqttEnabled == true)
+  {
+    DbgSerial.println("init MQTT");
+    MqttClient.setServer(mqtt_server, mqtt_port);
+    MqttClient.connect(mqtt_clientId, mqtt_user, mqtt_pass);
+  }
+  else
+  {
+    DbgSerial.println("MQTT disabled in main.h");
+  }
+
+  DbgSerial.println("pre init OTA");
+  otaMessage = "WS is in normal operation mode. For updating the firmware please enter OTA update mode and connect to the OTA Hotspot";
+  server.on("/", []() { server.send(200, "text/plain", otaMessage);});
   server.begin();
 }
 
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) 
+{
+  DbgSerial.println("Disconnected from Wi-Fi.");
   wifiReconnectTimer.once(2, connectToWifi);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-void setup() 
-{
-  pinMode(LED_PIN, OUTPUT);
-
-  Serial.begin(ComBaud);
-  WsSerial.begin(WsBaud);
-
-  Serial.println();
-  espClientSec.setInsecure();   //allow unsecure connections (don't compare certificate)
-
-
-
-  wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
-  wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
-  connectToWifi();
-  
-
-  Serial.println("WS Router begin");
-  WsRouter.begin(&WsBuffer, &WsData, &WsSerial, &NtpClient);
-
-
-  Serial.println("Init complete");
-}
-
-void loop() 
-{
-
-  if (WiFi.isConnected())
-  {
-    //Ntp client will update every 60 seconds
-    NtpClient.update();
-
-    if (serialTimestamp + 10000 < millis()){
-        Serial.print(NtpClient.getFormattedTime());
-        Serial.print("  ");
-        Serial.println(WiFi.localIP());
-        serialTimestamp = millis();
-    }
-
-
-    //update mqtt client
-    if (mqttEnabled == true)
-    {
-      MqttClient.loop();
-    }
-  }
-
-  //read all bytes to ring buffer
-  while(WsSerial.available() > 0)
-  {
-    char input = WsSerial.read();
-    WsBuffer.add(input);
-  }
-
-  //weather station routing and responses
-  WsRouter.route();
-
-  //if WsData has new data, print to debug Serial
-  if(WsData.available())
-  {
-    //convert string to char array for publishing
-    String JsonData = WsData.toJson(true);
-    char arr[JsonData.length()+1];
-    JsonData.toCharArray(arr, JsonData.length()+1);
-
-    //print weather data to debug console
-    Serial.println(WsData.toString());
-
-
-    if (WiFi.isConnected())
-    {
-
-      if (mqttEnabled == true)
-      {
-        //publish to mqtt
-        if(MqttClient.connected())
-        {
-          MqttClient.publish(mqtt_topic, arr);
-        }
-        else
-        {
-          MqttClient.connect(mqtt_clientId, mqtt_user, mqtt_pass);
-        }
-      }
-    }
-  }
-
-
-
-
-  //if ota service has to init
-  if(WsRouter.initOTA() && otaRunning == false)
-  {
-    Serial.println("OTA requested");
-    initOtaTime = millis();
-
-    //close WiFi
-    Serial.println("Disconnect WiFi");
-
-    wifiDisconnectHandler = NULL;
-
-    if (WiFi.isConnected())
-    {
-      WiFi.disconnect(true);
-      while (WiFi.status() == WL_CONNECTED) {
-        delay(500);
-        Serial.println("Wait for disconnect...");
-      } 
-    }
-
-    Serial.println("Start WiFi OTA hotspot");
-
-    otaMessage = "WS is in OTA update mode. For firmware update please go to http://192.168.1.1/update";
-
-    //setup hotspot
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-    WiFi.softAP(hotSpotSsid, hotSpotPassword);
-
-    //init OTA server
-    AsyncElegantOTA.begin(&server);    // Start ElegantOTA
-    Serial.println("OTA started");
-    otaRunning = true;
-  }
-
-
-
-  if(initOtaTime != 0 && (initOtaTime + initOtaTimeout) < millis())
-  {
-    otaRunning = false;
-    initOtaTime = 0;
-    Serial.println("Reset esp8266");
-    ESP.restart();
-  }
-
-
-
 }
--- WS2900_ESP8266/src/main.h
+++ WS2900_ESP8266/src/main.h
@@ -1,7 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+//https://patorjk.com/software/taag/#p=display&h=0&v=3&f=Bigfig&t=
 
-//##    include stuff   ####
+//-------------------------------
+// ___    __       __ _ 
+//  | |\|/  |  | ||_ | \
+// _|_| |\__|__|_||__|_/
+//-------------------------------
 #include "Arduino.h"
 #include "Ws2900Data.h"
 #include "SoftwareSerial.h"
@@ -14,36 +19,70 @@
 #include "PubSubClient.h"
 
 #include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
 #include <Ticker.h>
+#include <ESP8266WebServer.h>
+#include <ElegantOTA.h>
 
-
-//##    define stuff    ####
+//-------------------------------
+//  _  __ _____   ______ _     __
+// | \|_ |_  | |\| |  | / \|\|(_ 
+// |_/|__|  _|_| |_|_ | \_/| |__)
+//-------------------------------
 #define LED_PIN 4
+#define OTA_PIN 14
 
-#define WsTxPin D3
-#define WsRxPin D2
-#define WsBaud 9600
+#define WsTxPin 
+#define WsRxPin 
+#define WsBaud  9600
 
-#define ComBaud 115200
+#define DbgTxPin    12 
+#define DbgRxPin    16 //not used only debug
+#define DbgBaud     115200
 
 #define bufSize 300
 
-unsigned long initOtaTime = 0;
-long long int initOtaTimeout = 3600000;
-//long long int initOtaTimeout = 240000;
-
+//-------------------------------
+//  _  _  _ ___ _ ___\ / _  __
+// |_)|_)/ \ | / \ |  Y |_)(_ 
+// |  | \\_/ | \_/ |  | |  __)
+//-------------------------------
+void connectToWifi();
+void onWifiConnect(const WiFiEventStationModeGotIP& event);
+void onWifiDisconnect(const WiFiEventStationModeDisconnected& event);
+void createAp();
+//-------------------------------
+//    ___ __ __
+// |V| | (_ /  
+// | |_|___)\__
+//-------------------------------
 long long int serialTimestamp = 0;
 
+
+//-------------------------------
+//    ___ _____
+// | | | |_  | 
+// |^|_|_|  _|_
+//-------------------------------
 const char *ssid     = "Hotspot";
 const char *password = "Passwort";
 
-bool otaRunning = false;
 
-const char* hotSpotSsid = "WS-OTA-Hotspot";
-const char* hotSpotPassword = "123456789";
-IPAddress apIP(192, 168, 1, 1);
+//-------------------------------
+//  _ ___ _        _  _  _ ___ __
+// / \ | |_|---| ||_)| \|_| | |_ 
+// \_/ | | |   |_||  |_/| | | |__
+//-------------------------------
+
+const char* ApSsid      = "WS-OTA-Hotspot";
+const char* ApPassword  = "123456789";
+
+IPAddress local_IP(192, 168, 1, 100);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+unsigned long initOtaTime = 0;
+unsigned long initOtaTimeout = 3600000;
+bool otaRunning = false;
 
 WiFiEventHandler wifiConnectHandler;
 WiFiEventHandler wifiDisconnectHandler;
@@ -51,32 +90,43 @@ Ticker wifiReconnectTimer;
 
 String otaMessage = "";
 
+uint32_t otaEnabled = 0;
+#define otaEnalbedOff 0
 
-//define MQTT stuff
+//-------------------------------
+//     _ ______
+// |V|/ \ |  | 
+// | |\_X |  | 
+//-------------------------------
 const char*     mqtt_server     = "se-homecontrol.informatik.tha.de";
 const uint16_t  mqtt_port       = 15;
 const char*     mqtt_clientId   = "Noerdlingen_11";
 const char*     mqtt_user       = "Noerdlingen_1";
 const char*     mqtt_pass       = "Ryn6Kj1MDG";
+const char*     mqtt_topic      = "se/i2projekt/noerdlingen/wetterstation";
+const bool      mqttEnabled     = true;
 
-const char* mqtt_topic          = "se/i2projekt/noerdlingen/wetterstation";
-const char* mqtt_topic2         = "se/i2projekt/noerdlingen/WS";
+//-------------------------------
+//  _  _     __ _____ __
+// / \|_)  ||_ /   | (_ 
+// \_/|_)\_||__\__ | __)
+//-------------------------------
 
-const bool mqttEnabled = true;
-
-//##    objects         ####    
 Ws2900Data      WsData;
-SoftwareSerial  WsSerial(WsRxPin, WsTxPin); // RX, TX
+
+HardwareSerial  WsSerial(0);
+SoftwareSerial  DbgSerial(DbgRxPin, DbgTxPin);
+
 router          WsRouter;
 ringbuffer      WsBuffer;
 
 WiFiUDP         NtpUdp;
 NTPClient       NtpClient(NtpUdp, "time.fh-augsburg.de");
-//NTPClient       NtpClient(NtpUdp, "162.159.200.1");
 
 WiFiClientSecure    espClientSec;
 PubSubClient        MqttClient(espClientSec);
 
-AsyncWebServer server(80);
+ESP8266WebServer server(80);
+
 
 #endif 
